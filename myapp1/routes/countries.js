59753f64655b3c1537d2e899c4813afcbe868e58
Ajay Tanpure <ajaytanpure92@gmail.com>
2018-05-21 14:33:45 -0700

    cleanup

--- myapp1/routes/countries.js
+++ myapp1/routes/countries.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
 	result = {} //to hold the result in callback
 	request('https://raw.githubusercontent.com/Abhijith-Nagaraja/all-countries-and-cities-json/master/countries.json', function(err1, res1, body) {  
     	result = body;
-    	console.log(result);
     	res.send(result);
 	});
 });
